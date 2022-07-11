3d6cb6f8a805c5d536bfe515a181d4fac3ff924b
Cong Wang (engaging) <congwang@mit.edu>
2022-07-11 11:31:24 -0400

    update

--- hp1a25/martini/MARTINI.bash
+++ hp1a25/martini/MARTINI.bash
@@ -1,47 +1,47 @@
-martinize2 -f molecules/mole_0.pdb -nt -o molecules_MARTINI/PROA_0.top -ff martini3001 -x molecules_MARTINI/mole_0.pdb
-martinize2 -f molecules/mole_1.pdb -nt -o molecules_MARTINI/PROA_1.top -ff martini3001 -x molecules_MARTINI/mole_1.pdb
-martinize2 -f molecules/mole_2.pdb -nt -o molecules_MARTINI/PROA_2.top -ff martini3001 -x molecules_MARTINI/mole_2.pdb
-martinize2 -f molecules/mole_3.pdb -nt -o molecules_MARTINI/PROA_3.top -ff martini3001 -x molecules_MARTINI/mole_3.pdb
-martinize2 -f molecules/mole_4.pdb -nt -o molecules_MARTINI/PROA_4.top -ff martini3001 -x molecules_MARTINI/mole_4.pdb
-martinize2 -f molecules/mole_5.pdb -nt -o molecules_MARTINI/PROA_5.top -ff martini3001 -x molecules_MARTINI/mole_5.pdb
-martinize2 -f molecules/mole_6.pdb -nt -o molecules_MARTINI/PROA_6.top -ff martini3001 -x molecules_MARTINI/mole_6.pdb
-martinize2 -f molecules/mole_7.pdb -nt -o molecules_MARTINI/PROA_7.top -ff martini3001 -x molecules_MARTINI/mole_7.pdb
-martinize2 -f molecules/mole_8.pdb -nt -o molecules_MARTINI/PROA_8.top -ff martini3001 -x molecules_MARTINI/mole_8.pdb
-martinize2 -f molecules/mole_9.pdb -nt -o molecules_MARTINI/PROA_9.top -ff martini3001 -x molecules_MARTINI/mole_9.pdb
-martinize2 -f molecules/mole_10.pdb -nt -o molecules_MARTINI/PROA_10.top -ff martini3001 -x molecules_MARTINI/mole_10.pdb
-martinize2 -f molecules/mole_11.pdb -nt -o molecules_MARTINI/PROA_11.top -ff martini3001 -x molecules_MARTINI/mole_11.pdb
-martinize2 -f molecules/mole_12.pdb -nt -o molecules_MARTINI/PROA_12.top -ff martini3001 -x molecules_MARTINI/mole_12.pdb
-martinize2 -f molecules/mole_13.pdb -nt -o molecules_MARTINI/PROA_13.top -ff martini3001 -x molecules_MARTINI/mole_13.pdb
-martinize2 -f molecules/mole_14.pdb -nt -o molecules_MARTINI/PROA_14.top -ff martini3001 -x molecules_MARTINI/mole_14.pdb
-martinize2 -f molecules/mole_15.pdb -nt -o molecules_MARTINI/PROA_15.top -ff martini3001 -x molecules_MARTINI/mole_15.pdb
-martinize2 -f molecules/mole_16.pdb -nt -o molecules_MARTINI/PROA_16.top -ff martini3001 -x molecules_MARTINI/mole_16.pdb
-martinize2 -f molecules/mole_17.pdb -nt -o molecules_MARTINI/PROA_17.top -ff martini3001 -x molecules_MARTINI/mole_17.pdb
-martinize2 -f molecules/mole_18.pdb -nt -o molecules_MARTINI/PROA_18.top -ff martini3001 -x molecules_MARTINI/mole_18.pdb
-martinize2 -f molecules/mole_19.pdb -nt -o molecules_MARTINI/PROA_19.top -ff martini3001 -x molecules_MARTINI/mole_19.pdb
-martinize2 -f molecules/mole_20.pdb -nt -o molecules_MARTINI/PROA_20.top -ff martini3001 -x molecules_MARTINI/mole_20.pdb
-martinize2 -f molecules/mole_21.pdb -nt -o molecules_MARTINI/PROA_21.top -ff martini3001 -x molecules_MARTINI/mole_21.pdb
-martinize2 -f molecules/mole_22.pdb -nt -o molecules_MARTINI/PROA_22.top -ff martini3001 -x molecules_MARTINI/mole_22.pdb
-martinize2 -f molecules/mole_23.pdb -nt -o molecules_MARTINI/PROA_23.top -ff martini3001 -x molecules_MARTINI/mole_23.pdb
-martinize2 -f molecules/mole_24.pdb -nt -o molecules_MARTINI/PROA_24.top -ff martini3001 -x molecules_MARTINI/mole_24.pdb
-martinize2 -f molecules/mole_25.pdb -nt -o molecules_MARTINI/PROA_25.top -ff martini3001 -x molecules_MARTINI/mole_25.pdb
-martinize2 -f molecules/mole_26.pdb -nt -o molecules_MARTINI/PROA_26.top -ff martini3001 -x molecules_MARTINI/mole_26.pdb
-martinize2 -f molecules/mole_27.pdb -nt -o molecules_MARTINI/PROA_27.top -ff martini3001 -x molecules_MARTINI/mole_27.pdb
-martinize2 -f molecules/mole_28.pdb -nt -o molecules_MARTINI/PROA_28.top -ff martini3001 -x molecules_MARTINI/mole_28.pdb
-martinize2 -f molecules/mole_29.pdb -nt -o molecules_MARTINI/PROA_29.top -ff martini3001 -x molecules_MARTINI/mole_29.pdb
-martinize2 -f molecules/mole_30.pdb -nt -o molecules_MARTINI/PROA_30.top -ff martini3001 -x molecules_MARTINI/mole_30.pdb
-martinize2 -f molecules/mole_31.pdb -nt -o molecules_MARTINI/PROA_31.top -ff martini3001 -x molecules_MARTINI/mole_31.pdb
-martinize2 -f molecules/mole_32.pdb -nt -o molecules_MARTINI/PROA_32.top -ff martini3001 -x molecules_MARTINI/mole_32.pdb
-martinize2 -f molecules/mole_33.pdb -nt -o molecules_MARTINI/PROA_33.top -ff martini3001 -x molecules_MARTINI/mole_33.pdb
-martinize2 -f molecules/mole_34.pdb -nt -o molecules_MARTINI/PROA_34.top -ff martini3001 -x molecules_MARTINI/mole_34.pdb
-martinize2 -f molecules/mole_35.pdb -nt -o molecules_MARTINI/PROA_35.top -ff martini3001 -x molecules_MARTINI/mole_35.pdb
-martinize2 -f molecules/mole_36.pdb -nt -o molecules_MARTINI/PROA_36.top -ff martini3001 -x molecules_MARTINI/mole_36.pdb
-martinize2 -f molecules/mole_37.pdb -nt -o molecules_MARTINI/PROA_37.top -ff martini3001 -x molecules_MARTINI/mole_37.pdb
-martinize2 -f molecules/mole_38.pdb -nt -o molecules_MARTINI/PROA_38.top -ff martini3001 -x molecules_MARTINI/mole_38.pdb
-martinize2 -f molecules/mole_39.pdb -nt -o molecules_MARTINI/PROA_39.top -ff martini3001 -x molecules_MARTINI/mole_39.pdb
-martinize2 -f molecules/mole_40.pdb -nt -o molecules_MARTINI/PROA_40.top -ff martini3001 -x molecules_MARTINI/mole_40.pdb
-martinize2 -f molecules/mole_41.pdb -nt -o molecules_MARTINI/PROA_41.top -ff martini3001 -x molecules_MARTINI/mole_41.pdb
-martinize2 -f molecules/mole_42.pdb -nt -o molecules_MARTINI/PROA_42.top -ff martini3001 -x molecules_MARTINI/mole_42.pdb
-martinize2 -f molecules/mole_43.pdb -nt -o molecules_MARTINI/PROA_43.top -ff martini3001 -x molecules_MARTINI/mole_43.pdb
-martinize2 -f molecules/mole_44.pdb -nt -o molecules_MARTINI/PROA_44.top -ff martini3001 -x molecules_MARTINI/mole_44.pdb
-martinize2 -f molecules/mole_45.pdb -nt -o molecules_MARTINI/PROA_45.top -ff martini3001 -x molecules_MARTINI/mole_45.pdb
-martinize2 -f molecules/mole_46.pdb -nt -o molecules_MARTINI/PROA_46.top -ff martini3001 -x molecules_MARTINI/mole_46.pdb
+martinize2 -f molecules/mole_0.pdb -nt -o molecules_MARTINI/PROA_0.top -ff martini3001 -x molecules_MARTINI/mole_0.pdb -maxwarn 100
+martinize2 -f molecules/mole_1.pdb -nt -o molecules_MARTINI/PROA_1.top -ff martini3001 -x molecules_MARTINI/mole_1.pdb -maxwarn 100
+martinize2 -f molecules/mole_2.pdb -nt -o molecules_MARTINI/PROA_2.top -ff martini3001 -x molecules_MARTINI/mole_2.pdb -maxwarn 100
+martinize2 -f molecules/mole_3.pdb -nt -o molecules_MARTINI/PROA_3.top -ff martini3001 -x molecules_MARTINI/mole_3.pdb -maxwarn 100
+martinize2 -f molecules/mole_4.pdb -nt -o molecules_MARTINI/PROA_4.top -ff martini3001 -x molecules_MARTINI/mole_4.pdb -maxwarn 100
+martinize2 -f molecules/mole_5.pdb -nt -o molecules_MARTINI/PROA_5.top -ff martini3001 -x molecules_MARTINI/mole_5.pdb -maxwarn 100
+martinize2 -f molecules/mole_6.pdb -nt -o molecules_MARTINI/PROA_6.top -ff martini3001 -x molecules_MARTINI/mole_6.pdb -maxwarn 100
+martinize2 -f molecules/mole_7.pdb -nt -o molecules_MARTINI/PROA_7.top -ff martini3001 -x molecules_MARTINI/mole_7.pdb -maxwarn 100
+martinize2 -f molecules/mole_8.pdb -nt -o molecules_MARTINI/PROA_8.top -ff martini3001 -x molecules_MARTINI/mole_8.pdb -maxwarn 100
+martinize2 -f molecules/mole_9.pdb -nt -o molecules_MARTINI/PROA_9.top -ff martini3001 -x molecules_MARTINI/mole_9.pdb -maxwarn 100
+martinize2 -f molecules/mole_10.pdb -nt -o molecules_MARTINI/PROA_10.top -ff martini3001 -x molecules_MARTINI/mole_10.pdb -maxwarn 100
+martinize2 -f molecules/mole_11.pdb -nt -o molecules_MARTINI/PROA_11.top -ff martini3001 -x molecules_MARTINI/mole_11.pdb -maxwarn 100
+martinize2 -f molecules/mole_12.pdb -nt -o molecules_MARTINI/PROA_12.top -ff martini3001 -x molecules_MARTINI/mole_12.pdb -maxwarn 100
+martinize2 -f molecules/mole_13.pdb -nt -o molecules_MARTINI/PROA_13.top -ff martini3001 -x molecules_MARTINI/mole_13.pdb -maxwarn 100
+martinize2 -f molecules/mole_14.pdb -nt -o molecules_MARTINI/PROA_14.top -ff martini3001 -x molecules_MARTINI/mole_14.pdb -maxwarn 100
+martinize2 -f molecules/mole_15.pdb -nt -o molecules_MARTINI/PROA_15.top -ff martini3001 -x molecules_MARTINI/mole_15.pdb -maxwarn 100
+martinize2 -f molecules/mole_16.pdb -nt -o molecules_MARTINI/PROA_16.top -ff martini3001 -x molecules_MARTINI/mole_16.pdb -maxwarn 100
+martinize2 -f molecules/mole_17.pdb -nt -o molecules_MARTINI/PROA_17.top -ff martini3001 -x molecules_MARTINI/mole_17.pdb -maxwarn 100
+martinize2 -f molecules/mole_18.pdb -nt -o molecules_MARTINI/PROA_18.top -ff martini3001 -x molecules_MARTINI/mole_18.pdb -maxwarn 100
+martinize2 -f molecules/mole_19.pdb -nt -o molecules_MARTINI/PROA_19.top -ff martini3001 -x molecules_MARTINI/mole_19.pdb -maxwarn 100
+martinize2 -f molecules/mole_20.pdb -nt -o molecules_MARTINI/PROA_20.top -ff martini3001 -x molecules_MARTINI/mole_20.pdb -maxwarn 100
+martinize2 -f molecules/mole_21.pdb -nt -o molecules_MARTINI/PROA_21.top -ff martini3001 -x molecules_MARTINI/mole_21.pdb -maxwarn 100
+martinize2 -f molecules/mole_22.pdb -nt -o molecules_MARTINI/PROA_22.top -ff martini3001 -x molecules_MARTINI/mole_22.pdb -maxwarn 100
+martinize2 -f molecules/mole_23.pdb -nt -o molecules_MARTINI/PROA_23.top -ff martini3001 -x molecules_MARTINI/mole_23.pdb -maxwarn 100
+martinize2 -f molecules/mole_24.pdb -nt -o molecules_MARTINI/PROA_24.top -ff martini3001 -x molecules_MARTINI/mole_24.pdb -maxwarn 100
+martinize2 -f molecules/mole_25.pdb -nt -o molecules_MARTINI/PROA_25.top -ff martini3001 -x molecules_MARTINI/mole_25.pdb -maxwarn 100
+martinize2 -f molecules/mole_26.pdb -nt -o molecules_MARTINI/PROA_26.top -ff martini3001 -x molecules_MARTINI/mole_26.pdb -maxwarn 100
+martinize2 -f molecules/mole_27.pdb -nt -o molecules_MARTINI/PROA_27.top -ff martini3001 -x molecules_MARTINI/mole_27.pdb -maxwarn 100
+martinize2 -f molecules/mole_28.pdb -nt -o molecules_MARTINI/PROA_28.top -ff martini3001 -x molecules_MARTINI/mole_28.pdb -maxwarn 100
+martinize2 -f molecules/mole_29.pdb -nt -o molecules_MARTINI/PROA_29.top -ff martini3001 -x molecules_MARTINI/mole_29.pdb -maxwarn 100
+martinize2 -f molecules/mole_30.pdb -nt -o molecules_MARTINI/PROA_30.top -ff martini3001 -x molecules_MARTINI/mole_30.pdb -maxwarn 100
+martinize2 -f molecules/mole_31.pdb -nt -o molecules_MARTINI/PROA_31.top -ff martini3001 -x molecules_MARTINI/mole_31.pdb -maxwarn 100
+martinize2 -f molecules/mole_32.pdb -nt -o molecules_MARTINI/PROA_32.top -ff martini3001 -x molecules_MARTINI/mole_32.pdb -maxwarn 100
+martinize2 -f molecules/mole_33.pdb -nt -o molecules_MARTINI/PROA_33.top -ff martini3001 -x molecules_MARTINI/mole_33.pdb -maxwarn 100
+martinize2 -f molecules/mole_34.pdb -nt -o molecules_MARTINI/PROA_34.top -ff martini3001 -x molecules_MARTINI/mole_34.pdb -maxwarn 100
+martinize2 -f molecules/mole_35.pdb -nt -o molecules_MARTINI/PROA_35.top -ff martini3001 -x molecules_MARTINI/mole_35.pdb -maxwarn 100
+martinize2 -f molecules/mole_36.pdb -nt -o molecules_MARTINI/PROA_36.top -ff martini3001 -x molecules_MARTINI/mole_36.pdb -maxwarn 100
+martinize2 -f molecules/mole_37.pdb -nt -o molecules_MARTINI/PROA_37.top -ff martini3001 -x molecules_MARTINI/mole_37.pdb -maxwarn 100
+martinize2 -f molecules/mole_38.pdb -nt -o molecules_MARTINI/PROA_38.top -ff martini3001 -x molecules_MARTINI/mole_38.pdb -maxwarn 100
+martinize2 -f molecules/mole_39.pdb -nt -o molecules_MARTINI/PROA_39.top -ff martini3001 -x molecules_MARTINI/mole_39.pdb -maxwarn 100
+martinize2 -f molecules/mole_40.pdb -nt -o molecules_MARTINI/PROA_40.top -ff martini3001 -x molecules_MARTINI/mole_40.pdb -maxwarn 100
+martinize2 -f molecules/mole_41.pdb -nt -o molecules_MARTINI/PROA_41.top -ff martini3001 -x molecules_MARTINI/mole_41.pdb -maxwarn 100
+martinize2 -f molecules/mole_42.pdb -nt -o molecules_MARTINI/PROA_42.top -ff martini3001 -x molecules_MARTINI/mole_42.pdb -maxwarn 100
+martinize2 -f molecules/mole_43.pdb -nt -o molecules_MARTINI/PROA_43.top -ff martini3001 -x molecules_MARTINI/mole_43.pdb -maxwarn 100
+martinize2 -f molecules/mole_44.pdb -nt -o molecules_MARTINI/PROA_44.top -ff martini3001 -x molecules_MARTINI/mole_44.pdb -maxwarn 100
+martinize2 -f molecules/mole_45.pdb -nt -o molecules_MARTINI/PROA_45.top -ff martini3001 -x molecules_MARTINI/mole_45.pdb -maxwarn 100
+martinize2 -f molecules/mole_46.pdb -nt -o molecules_MARTINI/PROA_46.top -ff martini3001 -x molecules_MARTINI/mole_46.pdb -maxwarn 100
--- hp1a25/martini/split.py
+++ hp1a25/martini/split.py
@@ -1,5 +1,8 @@
 from tqdm import tqdm
 import os
+import sys
+
+N = int(sys.argv[1])
 
 os.makedirs('molecules', exist_ok=True)
 
@@ -15,7 +18,10 @@ with open('XXX_AA1.pdb', 'r') as f:
             mole_lis.append(mole_i)
             mole_i = []
         elif line_i[:3] == 'ATO':
-            mole_i.append(line_i)
+            index_new = int(line_i[22:27]) % N
+            if index_new == 0:
+                index_new = N 
+            mole_i.append(line_i[:22] + f'{index_new:>4d} ' + line_i[27:])
         line_i = f.readline()
 for i in tqdm(range(len(mole_lis))):
     with open(f'molecules/mole_{i}.pdb', 'w') as f:
@@ -23,9 +29,10 @@ for i in tqdm(range(len(mole_lis))):
         f.write(first_line)
         for line in mole_i:
             f.write(line)
+        f.write('END\n')
 os.makedirs('molecules_MARTINI', exist_ok=True)
 with open('MARTINI.bash', 'w') as f:
     for i in range(len(mole_lis)):
-        f.write(f'martinize2 -f molecules/mole_{i}.pdb -nt -o molecules_MARTINI/PROA_{i}.top -ff martini3001 -x molecules_MARTINI/mole_{i}.pdb\n')
+        f.write(f'martinize2 -f molecules/mole_{i}.pdb -nt -o molecules_MARTINI/PROA_{i}.top -ff martini3001 -x molecules_MARTINI/mole_{i}.pdb -maxwarn 100\n')
 
 
